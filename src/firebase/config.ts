bb482da427c09aa6c672ec3d3c81f1169f35a523
Rahupathy Mohan <rahu431@gmail.com>
2026-02-08 03:41:48 +0000

    Uncaught FirebaseError: Missing or insufficient permissions: The followi

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,5 +4,5 @@ export const firebaseConfig = {
   projectId: "studio-1712807649-250cc",
   storageBucket: "studio-1712807649-250cc.firebasestorage.app",
   messagingSenderId: "251063258498",
-  appId: "1:251063258498:web:7e66d3edf999db2ee6539b"
+  appId: "1:251063258498:web:7e66d3edf999db2ee6539b",
 };
